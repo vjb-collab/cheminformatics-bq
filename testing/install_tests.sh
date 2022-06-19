fc70f34b439eeb1241549e118ae112ba75c14016
vincent beltrani <vjbeltrani@google.com>
2022-06-19 23:48:29 +0000

    added draw svg, qed, etc

--- testing/install_tests.sh
+++ testing/install_tests.sh
@@ -57,55 +57,39 @@ gcloud beta functions add-iam-policy-binding "rdkit-molecular-descriptors" --reg
 
 bq query --use_legacy_sql=false --parameter="url::${CLOUD_TRIGGER_URL}" 'CREATE or REPLACE FUNCTION cheminformatics_test.rdkit_molecular_descriptors(smiles STRING) RETURNS STRING REMOTE WITH CONNECTION `us.cheminformatics-connection-test` OPTIONS (endpoint = @url)'
 
-## install substructure search stored procedure
+## install rdkit-draw-svg
 
-bq query --use_legacy_sql=false \
-'CREATE OR REPLACE PROCEDURE `cheminformatics_test.substructure_search`(fragment_smiles STRING)
-BEGIN
-declare json_return STRING;
-declare fragment_num_carbon, fragment_num_nitrogen, fragment_num_oxygen, fragment_num_fluorine, bit_count_fragment INT64;
-declare fragment_pattern_fp BYTES;
-declare fragment_bit_count INT64;
-set json_return=`cheminformatics_test.rdkit_pattern_fingerprint_test`(fragment_smiles);
-set fragment_pattern_fp = FROM_HEX(JSON_VALUE(json_return, "$.fp_pattern_long_as_binary_hex"));
-set fragment_num_carbon = CAST(JSON_VALUE(json_return, "$.num_carbon") as INT64);
-set fragment_num_oxygen = CAST(JSON_VALUE(json_return, "$.num_oxygen") as INT64);
-set fragment_num_nitrogen = CAST(JSON_VALUE(json_return, "$.num_nitrogen") as INT64);
-set fragment_num_fluorine = CAST(JSON_VALUE(json_return, "$.num_fluorine") as INT64);
-set fragment_bit_count = bit_count(fragment_pattern_fp);
-select smiles from `exports.savi_fp_clustered`
-where
-num_carbon >= fragment_num_carbon and
-num_nitrogen >= fragment_num_nitrogen and
-num_oxygen >= fragment_num_oxygen and
-num_fluorine >= fragment_num_fluorine and
-bit_count(fragment_pattern_fp & fp_pattern_as_binary) = fragment_bit_count
-and
-`cheminformatics_test.rdkit_substructure_match`(fragment_smiles, smiles);
-END'
+gcloud beta functions deploy rdkit-draw-svg --gen2 --region "us-east1" --entry-point rdkit_draw_svg --runtime python39 \
+    --trigger-http --quiet --memory=512MB --timeout=240s --max-instances=9000  > /dev/null
 
-## install morgan similarity search 
+CLOUD_TRIGGER_URL=$(gcloud beta functions describe rdkit-draw-svg --gen2 --region "us-east1" --format=json | jq -r '.serviceConfig.uri')
 
-bq query --use_legacy_sql=false \
-'CREATE OR REPLACE PROCEDURE `cheminformatics_test.morgan_similarity`(smiles STRING)
-BEGIN
-declare json_return STRING;
-declare morgan_fp BYTES;
-declare in_num_carbon, in_num_nitrogen, in_num_oxygen, in_num_fluorine INT64;
-set json_return=`cheminformatics_test.rdkit_pattern_fingerprint_test`(smiles);
-set morgan_fp = FROM_HEX(JSON_VALUE(json_return, "$.fp_morgan_as_binary_hex"));
-set in_num_carbon = CAST(JSON_VALUE(json_return, "$.num_carbon") as INT64);
-set in_num_oxygen = CAST(JSON_VALUE(json_return, "$.num_oxygen") as INT64);
-set in_num_nitrogen = CAST(JSON_VALUE(json_return, "$.num_nitrogen") as INT64);
-set in_num_fluorine = CAST(JSON_VALUE(json_return, "$.num_fluorine") as INT64);
- select smiles from `exports.savi_fp_clustered`
-where
-num_carbon in (in_num_carbon-4, in_num_carbon - 3, in_num_carbon - 2, in_num_carbon - 1, in_num_carbon, in_num_carbon + 1, in_num_carbon + 2, in_num_carbon+3, in_num_carbon+4)
-and
-num_oxygen in (in_num_oxygen-4, in_num_oxygen - 3, in_num_oxygen - 2, in_num_oxygen - 1, in_num_oxygen, in_num_oxygen + 1, in_num_oxygen + 2, in_num_oxygen+3, in_num_oxygen+4)
-and
-bit_count(morgan_fp & fp_morgan_as_binary) / bit_count(morgan_fp || fp_morgan_as_binary) > 0.3;
-END'
+gcloud beta functions add-iam-policy-binding "rdkit-draw-svg" --region "us-east1" --member=serviceAccount:${SERVICE_ACCOUNT} --role=${PERM} --gen2
+
+bq query --use_legacy_sql=false --parameter="url::${CLOUD_TRIGGER_URL}" 'CREATE or REPLACE FUNCTION cheminformatics_test.rdkit_draw_svg(smiles STRING) RETURNS STRING REMOTE WITH CONNECTION `us.cheminformatics-connection-test` OPTIONS (endpoint = @url)'
+
+## install rdkit-smiles-to-inchi
+
+gcloud beta functions deploy rdkit-smiles-to-inchi --gen2 --region "us-east1" --entry-point rdkit_smiles_to_inchi --runtime python39 \
+    --trigger-http --quiet --memory=512MB --timeout=240s --max-instances=9000  > /dev/null
+
+CLOUD_TRIGGER_URL=$(gcloud beta functions describe rdkit-smiles-to-inchi --gen2 --region "us-east1" --format=json | jq -r '.serviceConfig.uri')
+
+gcloud beta functions add-iam-policy-binding "rdkit-smiles-to-inchi" --region "us-east1" --member=serviceAccount:${SERVICE_ACCOUNT} --role=${PERM} --gen2
+
+bq query --use_legacy_sql=false --parameter="url::${CLOUD_TRIGGER_URL}" 'CREATE or REPLACE FUNCTION cheminformatics_test.rdkit_smiles_to_inchi(smiles STRING) RETURNS STRING REMOTE WITH CONNECTION `us.cheminformatics-connection-test` OPTIONS (endpoint = @url)'
+
+## install rdkit-qed
+
+gcloud beta functions deploy rdkit-qed --gen2 --region "us-east1" --entry-point rdkit_qed --runtime python39 \
+    --trigger-http --quiet --memory=512MB --timeout=240s --max-instances=9000  > /dev/null
+
+CLOUD_TRIGGER_URL=$(gcloud beta functions describe rdkit-qed --gen2 --region "us-east1" --format=json | jq -r '.serviceConfig.uri')
+
+gcloud beta functions add-iam-policy-binding "rdkit-qed" --region "us-east1" --member=serviceAccount:${SERVICE_ACCOUNT} --role=${PERM} --gen2
+
+bq query --use_legacy_sql=false --parameter="url::${CLOUD_TRIGGER_URL}" 'CREATE or REPLACE FUNCTION cheminformatics_test.rdkit_qed(smiles STRING) RETURNS STRING REMOTE WITH CONNECTION `us.cheminformatics-connection-test` OPTIONS (endpoint = @url)'
+
 
 ## wait one minute for permissions to propagate
 echo "Waiting for permissions to propagate ..."
--- testing/main.py
+++ testing/main.py
@@ -19,30 +19,20 @@ def rdkit_pattern_fingerprint_test(request):
                 
                 mol = Chem.MolFromSmiles(smiles)
                 
-                #fp_pattern_short = Chem.PatternFingerprint(mol, 11, tautomerFingerprints=True)
-                #fp_pattern_short_as_binary = DataStructs.BitVectToBinaryText(fp_pattern_short)
-                #fp_pattern_short_as_binary_hex = fp_pattern_short_as_binary.hex()
-                #fp_pattern_short_as_int = int(fp_pattern_short.ToBitString(),2)
-
                 fp_pattern_long = Chem.PatternFingerprint(mol, tautomerFingerprints=True)
                 fp_pattern_long_as_binary = DataStructs.BitVectToBinaryText(fp_pattern_long)
                 fp_pattern_long_as_binary_hex = fp_pattern_long_as_binary.hex()
-                #fp_pattern_long_as_int = int(fp_pattern_long.ToBitString(),2)
+               
 
                 fp_morgan = rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius = 2, useChirality=True, nBits = 2048)
                 fp_morgan_as_binary = DataStructs.BitVectToBinaryText(fp_morgan)
                 fp_morgan_as_binary_hex = fp_morgan_as_binary.hex()
-                #fp_morgan_as_int = int(fp_morgan.ToBitString(),2)
+                
 
                 fingerprints = {}
 
-                #fingerprints["fp_pattern_short_as_binary_hex"] = fp_pattern_short_as_binary_hex
                 fingerprints["fp_pattern_long_as_binary_hex"] = fp_pattern_long_as_binary_hex
                 fingerprints["fp_morgan_as_binary_hex"] = fp_morgan_as_binary_hex
-
-                #fingerprints["fp_pattern_short_as_int"]= fp_pattern_short_as_int
-                #fingerprints["fp_pattern_long_as_int"] = fp_pattern_long_as_int
-                #fingerprints["fp_morgan_as_int"] = fp_morgan_as_int
 
                 # count carbons
                 q = rdqueries.AtomNumEqualsQueryAtom(6)
@@ -139,3 +129,69 @@ def rdkit_molecular_descriptors(request):
         return return_json
     except Exception:
         return json.dumps( { "errorMessage": 'something unexpected in input' } ), 400
+
+def rdkit_draw_svg(request):
+    from rdkit.Chem.Draw import rdMolDraw2D
+    import re
+    pattern = re.compile("<\?xml.*\?>")
+    try:
+        return_value = []
+        request_json = request.get_json()
+        calls = request_json['calls']
+        
+        for call in calls:       
+            smiles = call[0]            
+            try:
+                mc = Chem.MolFromSmiles(smiles)
+                drawer = rdMolDraw2D.MolDraw2DSVG(*(450,150))
+                drawer.DrawMolecule(mc)
+                drawer.FinishDrawing()
+                svg = drawer.GetDrawingText().replace('svg:', '')
+                svg = re.sub(pattern, '', svg)
+                return_value.append(svg)
+            except:
+                return_value.append("")
+
+        return_json = json.dumps( { "replies" :  return_value} ), 200
+        return return_json
+    except Exception:
+        return json.dumps( { "errorMessage": 'something unexpected in input' } ), 400
+
+def rdkit_smiles_to_inchi(request):
+    try:
+        return_value = []
+        request_json = request.get_json()
+        calls = request_json['calls']
+        
+        for call in calls:     
+            smiles = call[0]  
+            try:
+                mol = Chem.MolFromSmiles(smiles)
+                return_value.append(Chem.MolToInchi(mol))
+            except:
+                return_value.append("")
+
+        return_json = json.dumps( { "replies" :  return_value} ), 200
+        return return_json
+    except Exception:
+        return json.dumps( { "errorMessage": 'something unexpected in input' } ), 400
+
+def rdkit_qed(request):
+    from rdkit.Chem import QED
+    try:
+        return_value = []
+        request_json = request.get_json()
+        calls = request_json['calls']
+        for call in calls:     
+            smiles = call[0]  
+            try:
+                mol = Chem.MolFromSmiles(smiles)
+                return_value.append(str(QED.qed(mol)))
+            except:
+                return_value.append("")
+
+        return_json = json.dumps( { "replies" :  return_value} ), 200
+        return return_json
+    except Exception:
+        return json.dumps( { "errorMessage": 'something unexpected in input' } ), 400
+
